b5d46c54312eaf52b552d64c089b47f26b32cbe4
iGyalchester <borisgerard333@gmail.com>
2021-10-14 13:29:49 -0400

    spelling in question2

--- src/main/java/Assignment/Question_2.java
+++ src/main/java/Assignment/Question_2.java
@@ -4,10 +4,10 @@ public class Question_2 {
 	public static void main(String[] args) {
 		int[] numArray = {10, 3, 4, 67};
 
-		System.out.println(secondAndThirdPlusAddAndMiltiplyByFirst(numArray));
+		System.out.println(secondAndThirdPlusAddAndMultiplyByFirst(numArray));
 	}
 
-	public static int secondAndThirdPlusAddAndMiltiplyByFirst(int[] num) {
+	public static int secondAndThirdPlusAddAndMultiplyByFirst(int[] num) {
 		int num1 = 0;
 		int num2 = 0;
 		for (int i : num){
